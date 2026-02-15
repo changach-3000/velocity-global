11ceb7c371883467208e393fa1e10e8bf3ea0738
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2026-02-15 22:53:22 +0300

    remove encrption from superuser migration

--- apps/pocketbase/pb_migrations/1764579159_create_superuser.js
+++ apps/pocketbase/pb_migrations/1764579159_create_superuser.js
@@ -1,12 +1,8 @@
 /// <reference path="../pb_data/types.d.ts" />
+/// <reference path="../pb_data/types.d.ts" />
 migrate((app) => {
-    const encryptionKey = $os.getenv("PB_ENCRYPTION_KEY")
-    
-    const emailEncrypted = $os.getenv("PB_SUPERUSER_EMAIL")
-    const passwordEncrypted = $os.getenv("PB_SUPERUSER_PASSWORD")
-
-    const email = $security.decrypt(emailEncrypted, encryptionKey)
-    const password = $security.decrypt(passwordEncrypted, encryptionKey)
+    const email = $os.getenv("PB_SUPERUSER_EMAIL")
+    const password = $os.getenv("PB_SUPERUSER_PASSWORD")
     
     const superusers = app.findCollectionByNameOrId("_superusers")
     const record = new Record(superusers)
