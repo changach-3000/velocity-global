dd0230a0a0b8f96760f6a7482f9240412280d598
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2026-02-17 13:52:45 +0300

    change login credentials in paystack

--- apps/api/src/routes/paystack.js
+++ apps/api/src/routes/paystack.js
@@ -362,7 +362,7 @@ const authenticatePocketBase = async () => {
   try {
     if (!pb.authStore.isValid) {
       console.log(`[${getTimestamp()}] [Paystack] Authenticating as superuser...`);
-      await pb.admins.authWithPassword(
+      await pb.collection('_superusers').authWithPassword(
         process.env.POCKETBASE_ADMIN_EMAIL,
         process.env.POCKETBASE_ADMIN_PASSWORD
       );
